b81fce1972c48f422d6944c55794e09a1f977e4e
Andrew <andrew-6676@ya.ru>
2019-03-09 12:28:21 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let opens = '', closes = '';
+    bracketsConfig.forEach( (cfg) => {
+        opens += cfg[0];
+        closes += cfg[1];
+    });
+
+    let stack = [];
+
+    for (let i in str) {
+        let s = str[i];
+        opn = !!~opens.indexOf(s);
+        cls = !!~closes.indexOf(s);
+        if (opn && cls) { // если одинаковая открывающая и закрывающая скобки
+            if (s === stack[stack.length-1]) {
+                stack.pop(s);
+            } else {
+                stack.push(s);
+            }
+        } else { // разные откр и закр скобки
+            if (opn) {
+                stack.push(s);
+            } else {
+                if (opens.indexOf(stack.pop()) !== closes.indexOf(s)) {
+                    return false;
+                }
+            }
+        }
+    }
+    return stack.length === 0;
+};
